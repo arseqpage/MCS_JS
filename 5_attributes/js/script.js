209a60e9be9522fd89b7f13f36eddacc08ae509c
Arslan Zhazykpaev <arseqpage@gmail.com>
2020-06-15 18:19:31 +0300

    Attributes JS complete

---
+++ 5_attributes/js/script.js
@@ -0,0 +1,12 @@
+console.log('You are at ' + window.location);
+
+const image = document.querySelector('img');
+
+
+console.log(image.hasAttribute('alt'));
+image.removeAttribute('alt');
+console.log(image.hasAttribute('alt'));
+
+console.log(image.getAttribute('src'));
+image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Yandex_logo_ru.svg/1200px-Yandex_logo_ru.svg.png');
+console.log(image.getAttribute('src'));
