75667a6231eb814364372743dd4c450527cb30e3
Arslan Zhazykpaev <arseqpage@gmail.com>
2020-06-18 17:30:45 +0300

    attributes

--- 5_attributes/js/script.js
+++ 5_attributes/js/script.js
@@ -10,3 +10,4 @@ console.log(image.hasAttribute('alt'));
 console.log(image.getAttribute('src'));
 image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Yandex_logo_ru.svg/1200px-Yandex_logo_ru.svg.png');
 console.log(image.getAttribute('src'));
+
