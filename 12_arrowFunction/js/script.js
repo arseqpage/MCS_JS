262ff3b565a1f4a598b4b235c2fde260447907a0
Arslan Zhazykpaev <arseqpage@gmail.com>
2020-06-19 10:53:42 +0300

    arrowFunction

---
+++ 12_arrowFunction/js/script.js
@@ -0,0 +1,18 @@
+console.log('You are at ' + window.location);
+
+// let myText = function (element, text) {
+// 	element.innerHTML = text;
+// }
+
+// myText(document.body, 'Hiiiiiiii!');
+
+let myText2 = (element, text2) => {
+	element.innerHTML = text2;
+}
+h12 = document.querySelector('h1');
+console.log(h12);
+
+myText2(h12, 'Keeeekiiiiiiii!');
+
+
+// let myText2 = (element, text2) => element.innerHTML = text2;
