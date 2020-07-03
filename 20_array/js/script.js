5bc041c2e485e56b47876dd62778d5c13b28807e
Arslan <64689183+arseqpage@users.noreply.github.com>
2020-07-03 12:02:43 +0300

    array

---
+++ 20_array/js/script.js
@@ -0,0 +1,7 @@
+console.log('You are at ' + window.location);
+
+// const cools = document.getElementsByClassName('cool');
+// const cools = document.getElementsByTagName('div');
+const cools = document.querySelectorAll('.cool');
+console.log(cools[0]);
+cools[0].innerHTML = 'Hi im text im cool one'
