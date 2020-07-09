fd31f387fcb561ce6e102f4896c9e895d3c06e1c
Arslan <64689183+arseqpage@users.noreply.github.com>
2020-07-09 22:30:49 +0300

    objects complete

--- 19_object/js/script.js
+++ 19_object/js/script.js
@@ -1,20 +1,23 @@
 console.log('You are at ' + window.location);
 
-let person = {
-    name: "Arseq",
-    surname: "Zhazykpaev",
-    age: 23,
-    teacher: false,
-    sayHello: function () { return 'Hello ' + this.name }
+// let person = {
+//     name: "Arseq",
+//     surname: "Zhazykpaev",
+//     age: 23,
+//     teacher: false,
+//     sayHello: function () { return 'Hello ' + this.name }
+// }
+
+// console.log(person.name);
+// console.log(person.sayHello());
+
+// person.canCode = true;
+// console.log(person);
+
+// person.age = 22;
+// console.log(person);
+// console.log(person['surname']);
+
+let array = {
+    0: ''
 }
-
-console.log(person.name);
-console.log(person.sayHello());
-
-person.canCode = true;
-console.log(person);
-
-person.age = 22;
-console.log(person);
-console.log(person['surname']);
-
