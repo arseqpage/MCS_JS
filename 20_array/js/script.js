861926f0562a074a991b32007158a8734fd4294e
Arslan <64689183+arseqpage@users.noreply.github.com>
2020-07-09 22:31:00 +0300

    arrays

--- 20_array/js/script.js
+++ 20_array/js/script.js
@@ -2,6 +2,43 @@ console.log('You are at ' + window.location);
 
 // const cools = document.getElementsByClassName('cool');
 // const cools = document.getElementsByTagName('div');
-const cools = document.querySelectorAll('.cool');
-console.log(cools[0]);
-cools[0].innerHTML = 'Hi im text im cool one'
+// const cools = document.querySelectorAll('.cool');
+// console.log(cools[0]);
+// cools[0].innerHTML = 'Hi im text im cool one'
+
+let people = ['Vasya', 'Petya', 'Sasha'];
+console.log(people);
+
+console.log(people[0]);
+
+people[0] = 'Yura';
+console.log(people[0]);
+console.log(people);
+
+console.log(people.length);
+
+let bigArray = [
+    [1, 2, 3],
+    [true, false],
+    ['a', 'b', 'c']
+]
+
+console.log(bigArray[0][1]);
+let bigObject = {
+    names: ['Ars', 'Dima', 'Vitaly'],
+    grades: [
+        {
+            intern: 1,
+            junior: 2
+        },
+        {
+            middle: 3,
+            senior: 2
+        }
+
+    ]
+}
+
+console.log(bigObject.names[0]);
+
+console.log(bigObject.grades[1].middle);
